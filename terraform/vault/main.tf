b1b6ccb3e0b348bdae6ed6f8321b434a2d432c17
Andrew Krasnikov <andrewk@emotorwerks.com>
2024-07-18 13:52:31 -0700

    init

---
+++ terraform/vault/main.tf
@@ -0,0 +1,74 @@
+# Network and Firewall
+resource "google_compute_network" "vault_network" {
+  name                    = "vault-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vault_subnetwork" {
+  name          = "vault-subnetwork"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vault_network.id
+}
+
+resource "google_compute_firewall" "vault_firewall" {
+  name    = "vault-firewall"
+  network = google_compute_network.vault_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8200"]
+  }
+
+  source_ranges = [var.allowed_ip]
+}
+
+# Vault Instance
+resource "google_compute_instance" "vault_vm" {
+  name         = "vault-vm"
+  machine_type = var.machine_type
+  zone         = "${var.region}-b"  
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts" 
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.vault_subnetwork.id
+    access_config {
+      # If you want to access Vault directly over the internet.
+      # Note that this is for demonstration, consider load balancers in prod
+    }
+  }
+
+  # Install Vault (adjust if you use a package manager or custom installation)
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y curl unzip
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update && sudo apt-get install vault
+    
+    # Basic Vault config 
+    sudo mkdir -p /etc/vault.d
+    sudo cat <<EOF > /etc/vault.d/vault.hcl
+    storage "file" {
+      path = "/vault/data"
+    }
+    listener "tcp" {
+      address = "0.0.0.0:8200"
+      tls_disable = 1  # Only for DEMO
+    }
+    ui = true # Enable Vault UI
+    EOF
+    sudo systemctl enable vault
+    sudo systemctl start vault
+  EOT
+}
+
+output "vault_public_ip" {
+  value = google_compute_instance.vault_vm.network_interface.0.access_config.0.nat_ip
+}
---
+++ terraform/vault/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  type        = string
+}
+
+variable "region" {
+  type        = string
+  default     = "us-west1"
+}
+
+variable "machine_type" {
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "allowed_ip" {
+  type        = string
+}
