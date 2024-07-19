f716232c0369ba38842df8fcef43373c1f176545
Andrew Krasnikov <andrewk@emotorwerks.com>
2024-07-19 16:56:59 -0700

    added vault test pipeline

---
+++ terraform/vault-test/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+resource "random_id" "random_pet" {
+  byte_length = 4
+}
+
+output "random_pet" {
+  value = random_id.random_pet.hex
+}
+
+output "secret_value" {
+  value     = var.SHOW
+#   sensitive = true
+}
