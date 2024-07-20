2c4529978daa226325515acec8a88a41b2f03feb
Andrew Krasnikov <andrewk@emotorwerks.com>
2024-07-20 15:17:21 -0700

    added k8s infrustructure pipeline

--- terraform/website/main.tf
+++ terraform/website/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.50.0"
+    }
+  }
+}
+
+provider "google" {
+  # project = var.project_id
+  region  = var.region
+}
+
 # Random ID Generation for k8s
 resource "random_id" "instance_name_suffix" {
   byte_length = 4
